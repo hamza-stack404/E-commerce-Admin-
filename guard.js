adb9c89880a2e4789b22c48a53430015be6a4683
hamza-stack404 <hamzasajjad2032009@gmail.com>
2026-07-30 17:15:50 +0500

    new

--- guard.js
+++ guard.js
@@ -9,7 +9,7 @@ const supabase = createClient(
 const result = await supabase.auth.getSession()
 
 if (result.data.session === null) {
-    window.location.href = "login.html"
+    window.location.href = "index.html"
 
 } else {
     if (result.data.session.user.email !== "hamzasajjad2032009@gmail.com") {
